c392065cd7fed63d5a473cde1605d70ed5141f0a
HiraiShinya <shirai@bookingbook.co.jp>
2026-03-27 09:54:50 +0900

    Update client.js

--- js/client.js
+++ js/client.js
@@ -27,7 +27,6 @@ window.TrelloPowerUp.initialize({
     .then(function(values) {
       var cardFields = values[0].customFieldItems;
       var boardFields = values[1].customFields;
-      
       var badges = [];
 
       // もしカスタムフィールドが1つも入力されていなければ何もしない
@@ -35,7 +34,7 @@ window.TrelloPowerUp.initialize({
         return badges;
       }
 
-      // 💡 ここで「表示したい項目名」と「色・アイコン」を自由に設定します！
+      // 💡 ここに書いた【上からの順番】で、必ず左から右へバッジが並びます！
       // Trelloで使える色は: blue, green, orange, red, yellow, purple, pink, sky, lime, light-gray, black
       var displayRules = {
         'Ｎ作業開始年月日': { color: 'blue', icon: '📅 ' },
@@ -43,46 +42,45 @@ window.TrelloPowerUp.initialize({
         'Ｎ工予見ＮＯ': { color: 'green', icon: ' ' },
         'Ａ車型': { color: 'sky', icon: '🚚 ' },
         'Ｎ作業完了年月日': { color: 'red', icon: '📅 ' },
-        'Ｎ作業完了時刻': { color: 'red', icon: '⌚ ' },        
-        // 必要なものをどんどん下に追加できます！
+        'Ｎ作業完了時刻': { color: 'red', icon: '⌚ ' }
       };
 
-      // カードに入力されているデータ群をループ処理
-      cardFields.forEach(function(item) {
-        // IDから「本当の項目名」を探し出す
-        var fieldDef = boardFields.find(function(f) { return f.id === item.idCustomField; });
+      // Trelloの気まぐれな順番ではなく、上で決めたルールの順番通りに探してバッジを作る
+      Object.keys(displayRules).forEach(function(ruleName) {
+        var rule = displayRules[ruleName];
+
+        // 1. ボードの設定から、この名前の項目のIDを探す
+        var fieldDef = boardFields.find(function(f) { return f.name === ruleName; });
         if (!fieldDef) return;
 
-        var fieldName = fieldDef.name;
-        var rule = displayRules[fieldName];
+        // 2. カードに入力されたデータの中から、そのIDと一致するものを探す
+        var item = cardFields.find(function(i) { return i.idCustomField === fieldDef.id; });
+        if (!item) return; // 未入力ならスキップ
 
-        // もし上のルールに書いてある項目名と一致したら、バッジを作る！
-        if (rule) {
-          var displayValue = '';
+        var displayValue = '';
 
-          // テキストや数値、日付の場合
-          if (item.value) {
-            displayValue = item.value.text || item.value.number || item.value.date;
-            // 日付の場合は「2026-11-28T00:00:00.000Z」みたいになるので、最初の10文字（年-月-日）だけ切り取る
-            if (item.value.date) {
-              displayValue = displayValue.substring(0, 10);
-            }
-          } 
-          // ドロップダウン（選択肢）の場合
-          else if (item.idValue && fieldDef.options) {
-            var selectedOption = fieldDef.options.find(function(o) { return o.id === item.idValue; });
-            if (selectedOption && selectedOption.value) {
-              displayValue = selectedOption.value.text;
-            }
+        // テキストや数値、日付の場合
+        if (item.value) {
+          displayValue = item.value.text || item.value.number || item.value.date;
+          // 日付の場合は「2026-11-28T00:00:00.000Z」みたいになるので、最初の10文字（年-月-日）だけ切り取る
+          if (item.value.date) {
+            displayValue = displayValue.substring(0, 10);
           }
-
-          // データが空じゃなければバッジとして追加
-          if (displayValue) {
-            badges.push({
-              text: rule.icon + displayValue, // 「🚗 130ア 326」みたいになる
-              color: rule.color              // 指定した色を塗る
-            });
+        } 
+        // ドロップダウン（選択肢）の場合
+        else if (item.idValue && fieldDef.options) {
+          var selectedOption = fieldDef.options.find(function(o) { return o.id === item.idValue; });
+          if (selectedOption && selectedOption.value) {
+            displayValue = selectedOption.value.text;
           }
+        }
+
+        // データが空じゃなければバッジとして追加
+        if (displayValue) {
+          badges.push({
+            text: rule.icon + displayValue, 
+            color: rule.color              
+          });
         }
       });
 
